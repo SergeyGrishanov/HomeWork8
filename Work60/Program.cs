46053f9c159884f3c6a9f796fbb340dacf9f0a2a
Sergey <blackwhiterainbow@yandex.ru>
2023-02-12 13:45:02 +0300

    add Построчное выведение массива с добавлением индекса каждого элемента

---
+++ Work60/Program.cs
@@ -0,0 +1,71 @@
+﻿// Sформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+
+int[,,] GetArray(int x, int y, int z, int minValue, int maxValue)
+{
+    int[,,] result = new int[x,y,z];
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                result[i,j,k] = new Random().Next(minValue, maxValue + 1);
+            }
+            
+        }
+    }
+    return result;
+}
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.WriteLine($"[{i},{j},{k}] - {inArray[i, j, k]}");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] CreateArray(int x, int y, int z)
+{
+    int[,,] array = new int[x, y, z];
+    int[] values = new int[10];
+    int num = 10;
+    for (int i = 0; i < values.Length; i++)
+        values[i] = num
+        ++;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        int randomInd = new Random().Next(0, values.Length);
+        int temp = values[i];
+        values[i] = values[randomInd];
+        values[randomInd] = temp;
+    }
+
+    int numsIndex = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = values[numsIndex++];
+            }
+        }
+    }
+    return array;
+}
+
+Console.Clear();
+
+int[,,] array = GetArray(x: 2, y: 2, z: 2,  minValue: 1, maxValue: 10);
+PrintArray(array);
