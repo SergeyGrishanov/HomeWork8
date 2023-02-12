e193af9a764ada8da23104af6a1a5e00d696a716
Sergey <blackwhiterainbow@yandex.ru>
2023-02-12 13:46:53 +0300

    add Нахождение квадратного корня без библиотеки Math

---
+++ WorkSpec/Program.cs
@@ -0,0 +1,20 @@
+﻿//Написать функцию Sqrt(x) - квадратного корня, которая принимает на вход целочисленное значение x 
+//и возвращает целую часть квадратного корня от введенного числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите числа: ");
+double num = int.Parse(Console.ReadLine()!);
+
+double number = num;
+double result = 0;
+
+double a = 1;
+while (a <= number)
+{ 
+    
+    number -= a;
+    a += 2;
+    result+=1; 
+}
+Console.WriteLine($"Корень из числа {num} равен {result}");
