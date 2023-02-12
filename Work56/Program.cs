31c2121ad593993b9bedda8307d8df0bcd3ed563
Sergey <blackwhiterainbow@yandex.ru>
2023-02-12 13:42:53 +0300

    add Нахождение строки с наименьшей суммой элементов

---
+++ Work56/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void MinSumElements(int[,] matrix)
+{
+    int indexLine = 0;
+    int minSum = Int32.MaxValue;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];        
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            indexLine++;
+        }
+    }
+    Console.WriteLine("Минимальная сумма находится в строке под номером: " + (indexLine));
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+
+int[,] array = GetArray(m: 3, n: 4, minValue: 1, maxValue: 10);
+PrintArray(array);
+Console.WriteLine();
+
+MinSumElements(array);
+Console.WriteLine();
